e97e13015ed032ac890141bf1600d1aa8ab63774
Mykhailo Kurov <miklay89@gmail.com>
2023-02-03 21:18:37 +0000

    wm resize fix

--- dist/libs/watermark.js
+++ dist/libs/watermark.js
@@ -5,11 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const sharp_1 = __importDefault(require("sharp"));
 const watermark = async (waterMarkTemplate, file) => {
+    const meta = await (0, sharp_1.default)(file).metadata();
+    const wmH = parseInt((meta.height * 0.41).toFixed());
+    const wmImage = await (0, sharp_1.default)(waterMarkTemplate)
+        .resize(null, wmH)
+        .png()
+        .toBuffer();
     const newFile = await (0, sharp_1.default)(file)
         .composite([
         {
-            input: waterMarkTemplate,
-            density: 500,
+            input: wmImage,
         },
     ])
         .toFormat("png")
--- src/libs/watermark.ts
+++ src/libs/watermark.ts
@@ -5,11 +5,17 @@ const watermark = async (
   waterMarkTemplate: string,
   file: Buffer,
 ): Promise<Buffer> => {
+  const meta = await sharp(file).metadata();
+  // eslint-disable-next-line radix
+  const wmH = parseInt((meta.height! * 0.41).toFixed());
+  const wmImage = await sharp(waterMarkTemplate)
+    .resize(null, wmH)
+    .png()
+    .toBuffer();
   const newFile = await sharp(file)
     .composite([
       {
-        input: waterMarkTemplate,
-        density: 500,
+        input: wmImage,
       },
     ])
     .toFormat("png")
